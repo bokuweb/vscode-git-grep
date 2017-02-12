859b51516f15d0a8e17b83fb1d92125546542d96
bokuweb <bokuweb12@gmail.com>
2017-02-12 21:07:41 +0900

    use async

--- src/extension.ts
+++ src/extension.ts
@@ -6,42 +6,39 @@ import { window, commands, ExtensionContext, QuickPickItem, QuickPickOptions, wo
 
 const projectRoot = workspace.rootPath ? workspace.rootPath : '.';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vscode-git-grep" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = commands.registerCommand('extension.gitGrep', () => {
-        // The code you place here will be executed every time your command is executed
-        window.showInputBox({ prompt: 'search...' }).then((query) => {
+    (async () => {
+        const disposable = commands.registerCommand('extension.gitGrep', async () => {
+            const query = await window.showInputBox({ prompt: 'search...' })
             const command = quote(['git', 'grep', '-H', '-n', query]);
-            exec(command, { cwd: projectRoot }, (err, stdout, stderr) => {
+
+            exec(command, { cwd: projectRoot }, async (err, stdout, stderr) => {
                 const lines = stdout.split(/\n/);
-                console.log(lines[0])
-                window.showQuickPick(lines).then((l) => {
-                    const [file, line] = l.split(':');
-                    console.log(file)
-                    workspace.openTextDocument(projectRoot + '/' + file).then(doc => {
-                        console.log("openTextDocument success", doc.fileName);
-                        window.showTextDocument(doc).then(() => {
-                            const newSection = new Selection(~~line, 0, ~~line, 0);
-                            window.activeTextEditor.selection = newSection;
-                            commands.executeCommand('cursorUp');
-                        });
-                    });
-                });
+                console.log('err', err)
+                if (err) {
+                    console.log(stderr);
+                    window.showErrorMessage(stderr);
+                    return;
+                }
+                if (lines.length === 1 && lines[0] === '') {
+                    window.showInformationMessage('There are no items')
+                    return Promise.resolve();
+                }
+                const l = await window.showQuickPick(lines);
+                const [file, line] = l.split(':');
+                const doc = await workspace.openTextDocument(projectRoot + '/' + file);
+                await window.showTextDocument(doc);
+                const selection = new Selection(~~line, 0, ~~line, 0);
+                window.activeTextEditor.selection = selection;
+                commands.executeCommand('cursorUp');
                 context.subscriptions.push(disposable);
-            });
+            })
         });
+    })().catch((error) => {
+        window.showErrorMessage(error);
     });
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
 }
