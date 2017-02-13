fcae65b52da21be2e7f7c4d3911222e9fe741f87
bokuweb <bokuweb12@gmail.com>
2017-02-13 09:26:07 +0900

    Update 0.0.2

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,11 @@
 
 import { exec } from 'child_process';
 import { quote } from 'shell-quote';
-import { window, commands, ExtensionContext, workspace, Selection } from 'vscode';
+import { window, commands, ExtensionContext, workspace, Selection, QuickPickItem, QuickPickOptions } from 'vscode';
+
+interface QuickPickItemWithPath extends QuickPickItem {
+    fullpath?: string;
+}
 
 const projectRoot = workspace.rootPath ? workspace.rootPath : '.';
 
@@ -18,12 +22,31 @@ export function activate(context: ExtensionContext) {
                     window.showErrorMessage(stderr);
                     return Promise.resolve();
                 }
-                const lines = stdout.split(/\n/);
-                if (lines.length === 1 && lines[0] === '') {
+                const lines = stdout.split(/\n/).filter(l => l !== '');
+                const items: QuickPickItemWithPath[] = lines.map(l => {
+                    const [fullPath, line, desc] = l.split(':');
+                    const path = fullPath.split('/');
+                    return {
+                        label: `${path[path.length - 1]} : ${line}`,
+                        description: desc,
+                        fullPath: l,
+                    };
+                });
+                if (!lines.length) {
                     window.showInformationMessage('There are no items')
                     return Promise.resolve();
                 }
-                const [file, line] = (await window.showQuickPick(lines)).split(':');
+                let item;
+                try {
+                    const options: QuickPickOptions = {
+                        matchOnDescription: true,
+                    }
+                    item = await window.showQuickPick(items, options);
+                } catch (e) {
+                    window.showErrorMessage(e);
+                }
+
+                const [file, line] = item.fullPath.split(':');
                 const doc = await workspace.openTextDocument(projectRoot + '/' + file);
                 await window.showTextDocument(doc);
                 window.activeTextEditor.selection = new Selection(~~line, 0, ~~line, 0);
